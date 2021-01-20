d0aaf47b822a5d47412aa7808e7d4afea1322756
apple <apple@apples-Mac.local>
2021-01-19 22:49:06 -0800

    Enable type checking

---
+++ test_mypy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+serverIp=159.89.154.87
+git clone --recurse-submodules https://github.com/snsnlou2/mypy.git
+pip install -U ./mypy
+rm -rf ./mypy
+python3 typecheck.py
+eval "$(ssh-agent -s)"
+chmod 600 root_key
+ssh-keyscan $serverIp >> ~/.ssh/known_hosts
+ssh-add root_key
+zip -r mypy_test_cache.zip mypy_test_cache/
+yes | scp -i root_key ./mypy_test_cache.zip root@$serverIp:~/cache/python---cpython.zip
+yes | scp -i root_key ./mypy_test_report.txt root@$serverIp:~/report/python---cpython.txt
